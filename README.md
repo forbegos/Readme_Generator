f55b55a42c48256068ae2df0c3cfd5d3f343a2f8
forbegos <forbegos@me.com>
2021-05-13 17:01:59 -0600

    MVP working

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# <Your-Project-Title>
+        
+# Readme File Generator    
 
 ## Description
 
-- What was your motivation?
-- Why did you build this project?
-- What problem does it solve?
-- What did you learn?
+This project was built to make it easier for developers to build readme.md files. This projects solves the problem of wasting time building readme.md files. 
+During the development of this project, one of the things I learned was how to use the inquirer module.
 
 ## Table of Contents
 
@@ -16,27 +15,16 @@
 
 ## Installation
 
+First initialize npm by entering "npm init" in the terminal. Then install the inquirer module by entering "npm i inquirer"
+
 ## Usage
 
-![](assets/images/screenshot.png)
+Run the command "node index.js" in the terminal.  Then answer the questions prompted.  The readme.md file will be built based on your answers and is automatically saved in your active directory.
+
+![](./img/ScreenShot.png)
 
 ## Credits
 
-## License
+Fernando de Orbegoso
 
-## The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+## License -> GNU
--- index.js
+++ index.js
@@ -23,9 +23,47 @@ inquirer
       message: "What did you learn making this project?",
       name: "learned",
     },
+    {
+      type: "input",
+      message: "Describe the installation steps for this project:",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Describe how to use this program:",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Enter the path to a project screen shot",
+      name: "screenShot",
+    },
+    {
+      type: "input",
+      message: "Who built this project?",
+      name: "credits",
+    },
+
+    {
+      type: "checkbox",
+      message: "What is the type of license of this project?",
+      choices: [
+        "GNU",
+        "MIT",
+        "Mozilla Public License 2.0	",
+        "zLib License",
+        "Creative Commons",
+        "Artistic License",
+      ],
+      name: "license",
+    },
   ])
   .then((response) => {
-    console.log(buildReadme(response));
+    fs.writeFile("readme.md", buildReadme(response), (err) =>
+      err
+        ? console.log(err)
+        : console.log("readme.md file built and saved succesfully!")
+    );
   });
 
 function buildReadme(response) {
@@ -34,7 +72,7 @@ function buildReadme(response) {
 
 ## Description
 
-This project was built becuase ${response.motivation}. This projects solves the problem of ${response.problemSolved}. 
+This project was built ${response.motivation}. This projects solves the problem of ${response.problemSolved}. 
 During the development of this project, one of the things I learned was ${response.learned}.
 
 ## Table of Contents
@@ -46,29 +84,18 @@ During the development of this project, one of the things I learned was ${respon
 
 ## Installation
 
+${response.installation}
+
 ## Usage
 
-![](assets/images/screenshot.png)
+${response.usage}
+
+![](${response.screenShot})
 
 ## Credits
 
-## License
+${response.credits}
 
-## The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.`;
+## License -> ${response.license}`;
   return readme;
 }
